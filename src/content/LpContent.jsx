8d51190145155cc69cb50c6d10fb55519a1ab772
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-07 11:45:13 -0300

    fix: LpContent

--- src/content/LpContent.jsx
+++ src/content/LpContent.jsx
@@ -5,7 +5,7 @@ import bgHeroImg from "../assets/imgs/hero/bgHero.webp";
 import imgFeatures from "../assets/imgs/features/imgServices.jpg";
 import imgDivisor from "../assets/imgs/features/imgDivisor.jpg";
 
-import aboutImg1 from "../assets/imgs/about/aboutImg.jpg";
+import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
 import aboutSocialImg from "../assets/imgs/about/aboutSocial.png";
 
 import imgSteps from "../assets/imgs/steps/imgSteps.jpg";
